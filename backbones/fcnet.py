b699dd185279cc9918b4f48a0f93ce454d29c66b
Son Le <42708725+SonAlexLe@users.noreply.github.com>
2023-11-30 16:52:02 +0100

    the index mask_method now supports unequal-size concepts by padding

--- backbones/fcnet.py
+++ backbones/fcnet.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn as nn
+import math
 
 from backbones.blocks import full_block, full_block_fw
 
@@ -30,7 +31,7 @@ class FCNet(nn.Module):
 def generate_simple_go_mask(x_dim, num_GOs=20):
     # Generate a simple mask by dividing the genes into num_GOs groups
     go_mask = []
-    assert x_dim % num_GOs == 0, "x_dim must be divisible by num_GOs"
+    assert x_dim % num_GOs == 0, f"x_dim must be divisible by num_GOs: {x_dim}, {num_GOs}"
     num_genes_per_go = x_dim // num_GOs
     for i in range(num_GOs):
         go_mask.append(list(range(i * num_genes_per_go, (i + 1) * num_genes_per_go)))
@@ -39,34 +40,40 @@ def generate_simple_go_mask(x_dim, num_GOs=20):
 
 class EnFCNet(nn.Module):
 
-    def __init__(self, x_dim, layer_dim=None, go_mask=None, mask_method="index", num_GOs=20, dropout=0.2):
+    def __init__(self, x_dim, layer_dim=None, go_mask=None, mask_method="index", dropout=0.2):
         super(EnFCNet, self).__init__()
 
         if go_mask is None:
-            self.go_mask = generate_simple_go_mask(x_dim=x_dim, num_GOs=num_GOs) # for testing
+            self.go_mask = generate_simple_go_mask(x_dim=x_dim, num_GOs=20) # for testing
         else:
             self.go_mask = go_mask
 
-        self.num_GOs = num_GOs
+        self.num_GOs = len(self.go_mask)
         self.masks = None
         self.n_concepts = self.num_GOs# + 1
         self.mask_method = mask_method
 
         if mask_method == "index":
-            assert x_dim % self.n_concepts == 0, f"xdim should be divisible by the number of concepts: {xdim}, {self.n_concepts}"
-            input_dim = x_dim // self.n_concepts
+            self.input_dim = self.compute_input_dim(x_dim)
         elif mask_method == "multiply":
-            input_dim = x_dim
+            self.input_dim = x_dim
         else:
             raise ValueError("Unsupported masking method: {}".format(mask_method))
 
-        self.conv1 = nn.Conv1d(input_dim, layer_dim[0], 1, bias=True)
+        self.conv1 = nn.Conv1d(self.input_dim, layer_dim[0], 1, bias=True)
         self.conv2 = nn.Conv1d(layer_dim[0], layer_dim[1], 1, bias=True)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout)
         self.bn1 = nn.BatchNorm1d(self.n_concepts)
         self.bn2 = nn.BatchNorm1d(self.n_concepts)
         self.final_feat_dim = layer_dim[1]
+
+    def compute_input_dim(self, x_dim):
+        if self.n_concepts == self.num_GOs + 1:
+            return x_dim # the final concept is the original input
+        num_genes_per_go = max([len(genes) for genes in self.go_mask]) # all GOs have different number of genes
+        num_genes_per_go = int(math.ceil(num_genes_per_go / 8) * 8) # make it divisible by 8
+        return num_genes_per_go
 
     def generate_masks(self, x):
         batch, num_genes = x.shape
@@ -77,30 +84,26 @@ class EnFCNet(nn.Module):
             self.masks[-1, :, :] = 1
 
     def get_masked_inputs(self, x):
-        assert self.mask_method in ["multiply", "index"], "Unsupported masking method: {}".format(method)
+        assert self.mask_method in ["multiply", "index"], "Unsupported masking method: {}".format(self.mask_method)
         batch, num_genes = x.shape
         if self.masks is None or self.masks.shape[1] != batch:
             self.generate_masks(x)
             self.masks = self.masks.to(x.device) # (n_concepts, batch, num_genes)
         if self.mask_method == "multiply":
             x = x.view(1, batch, -1)
-            # x after applying mask: (n_concepts, batch, numGenes)
-            x = self.masks * x
+            x = self.masks * x # (n_concepts, batch, numGenes)
         else: # we index the original x according to each concept in self.masks (bool tensor)
-            assert self.n_concepts == self.num_GOs and num_genes % self.num_GOs == 0, \
-                "This approach does not support unequal-size concepts yet"
-            num_genes_per_go = num_genes // self.num_GOs
-            x_new = torch.zeros(self.num_GOs, batch, num_genes_per_go, device=x.device)
+            # note input_dim is << num_genes (2866 genes in Tabula Muris)
+            x_new = torch.zeros(self.num_GOs, batch, self.input_dim, device=x.device)
             masks = self.masks.bool()
             for i in range(self.num_GOs):
                 mask_i = masks[i].view(batch, num_genes)
-                x_i = torch.masked_select(x, mask_i).view(batch, num_genes_per_go)
-                x_new[i] = x_i
+                x_i = torch.masked_select(x, mask_i).view(batch, -1)
+                x_new[i][:, :x_i.shape[1]] = x_i # pad with zeros
             x = x_new
         return x
 
     def forward(self, x):
-        batch, num_genes = x.shape
         # need to generate masks if the batch size change or
         x = self.get_masked_inputs(x) # (n_concepts, batch, numGenes)
         x = x.permute(1, 2, 0) # (batch, numGenes, n_concepts)
