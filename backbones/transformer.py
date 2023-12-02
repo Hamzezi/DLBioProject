22bce016e623c60008cf2857c3450b742b3f51b8
Hamzezi <hamza.laarous@epfl.ch>
2023-12-02 14:40:36 +0100

    remove code duplication within transformer

--- backbones/transformer.py
+++ backbones/transformer.py
@@ -43,8 +43,9 @@ class TransformerDecoderNet(nn.Module, ConceptNetMixin):
 
         self.final_feat_dim = self.input_dim
     
-    def forward(self, x):
-        x = self.get_masked_inputs(x) # (batch, n_concepts, numGenes)
+    def forward(self, x, mask=True):
+        if mask:
+            x = self.get_masked_inputs(x) # (batch, n_concepts, numGenes)
         concept_indices = torch.arange(self.n_decoder_concepts, device=x.device)
         concepts = self.concept_embeds(concept_indices).unsqueeze(0).repeat(x.shape[0], 1, 1)
         concepts = self.decoder(concepts, x)
@@ -57,29 +58,10 @@ class TransformerNet(nn.Module, ConceptNetMixin):
         super(TransformerNet, self).__init__()
         ConceptNetMixin.__init__(self, x_dim, go_mask=None, mask_method="index", num_GOs=20)
 
-        # encoder
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=self.input_dim, nhead=1, dim_feedforward=ffw_dim,
-            dropout=dropout, batch_first=True, norm_first=True
-        )
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=1)
-
-        # decoder
-        self.n_decoder_concepts = 10
-        self.concept_embeds = nn.Embedding(self.n_decoder_concepts, self.input_dim)
-        decoder_layer = nn.TransformerDecoderLayer(
-            d_model=self.input_dim, nhead=1, dim_feedforward=ffw_dim,
-            dropout=dropout, batch_first=True, norm_first=True
-        )
-        self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=1)
-
-        self.final_feat_dim = self.input_dim
+        self.encoder = TransformerEncoderNet(x_dim, layer_dim, ffw_dim, go_mask, dropout)
+        self.decoder = TransformerDecoderNet(x_dim, layer_dim, ffw_dim, go_mask, dropout)
 
     def forward(self, x):
-        x = self.get_masked_inputs(x)
         x = self.encoder(x)
-        concept_indices = torch.arange(self.n_decoder_concepts, device=x.device)
-        concepts = self.concept_embeds(concept_indices).unsqueeze(0).repeat(x.shape[0], 1, 1)
-        concepts = self.decoder(concepts, x)
-        return concepts
-
+        x = self.decoder(x, mask=False)
+        return x
