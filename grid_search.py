2247647ab9c847de3ecbf49c841c404714d8a468
Hamzezi <hamza.laarous@epfl.ch>
2023-12-03 14:12:57 +0100

    fix cmd error

--- grid_search.py
+++ grid_search.py
@@ -1,15 +1,21 @@
 import itertools
 import subprocess
 
+transformer_type_values = ["transformer_encoder", "transformer_decoder", "transformer"]
 num_layers_values = [1, 2, 3]
 nhead_values = [1, 2, 3]
 dropout_values = [0, 0.1, 0.2]
+dataset = "tabula_muris"
 
-param_combinations = list(itertools.product(num_layers_values, nhead_values, dropout_values))
+param_combinations = list(itertools.product(transformer_type_values,\
+                                            num_layers_values,\
+                                            nhead_values,\
+                                            dropout_values))
 
 for params in param_combinations:
-    num_layers, nhead, dropout = params
-    exp_name = f"transformer_decoder_{num_layers}_{nhead}_{dropout}"
-    cmd = f"python run.py exp.name={exp_name} method=transformer method.transformer_type=transformer_decoder\
-          transformer_args.num_layers={num_layers} transformer_args.nhead={nhead} transformer_args.dropout={dropout}"
-    subprocess.run(cmd, shell=True)
+    transformer, num_layers, nhead, dropout = params
+    exp_name = f"{transformer}_{num_layers}_{nhead}_{dropout}"
+    cmd = f"python run.py exp.name={exp_name} method=transformer method.transformer_type={transformer} {transformer}_args.num_layers={num_layers} {transformer}_args.nhead={nhead} {transformer}_args.dropout={dropout} dataset={dataset}"
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+    break
