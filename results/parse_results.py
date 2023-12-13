b6006bea82c3db0bacf4e416ad8b5b6e16b6f26a
Son Le <42708725+SonAlexLe@users.noreply.github.com>
2023-12-13 18:16:07 +0100

    code cleanup

--- results/parse_results.py
+++ results/parse_results.py
@@ -1,6 +1,6 @@
 import re, os
-import numpy as np
 import json
+import argparse
 
 
 def parse_results_file(file_path):
@@ -25,13 +25,9 @@ def parse_results_file(file_path):
             'model': model
         })
     
-    assert len(parsed_data) == 3, f"{file_path}" # train, val (ablation), test (baseline comparison)
+    assert len(parsed_data) == 3, f"wrong results.txt structure for {file_path}" # train, val (ablation), test (baseline comparison)
 
     return parsed_data
-
-
-def get_dev_acc(parsed_data):
-    return float(parsed_data[2]['accuracy'][:5])
 
 
 def get_all_exp_dirs(ckpt_dir):
@@ -43,76 +39,36 @@ def get_json_obj(parsed_data):
     return {
         "setting": parsed_data[0]['setting'],
         "model": parsed_data[0]['model'],
-        "train_acc": float(parsed_data[0]['accuracy'][:5]),
-        "val_acc": float(parsed_data[1]['accuracy'][:5]),
-        "test_acc": float(parsed_data[2]['accuracy'][:5]),
+        "train_acc": parsed_data[0]['accuracy'],
+        "val_acc": parsed_data[1]['accuracy'],
+        "test_acc": parsed_data[2]['accuracy'],
     }
 
 
-exp_dirs = get_all_exp_dirs("./checkpoints")
-exp_dirs = [p for p in exp_dirs if "transformer" in p]
-file_paths = [os.path.join(p, "results.txt") for p in exp_dirs]
-# print(len(file_paths))
-file_paths = [f for f in file_paths if os.path.exists(f)]
-# print(len(file_paths))
+def main(ckpt_dir, save_filename):
+    # get all the results.txt paths
+    exp_dirs = get_all_exp_dirs(ckpt_dir)
+    exp_dirs = [p for p in exp_dirs if "comet" in p]
+    file_paths = [os.path.join(p, "results.txt") for p in exp_dirs]
+    file_paths = [f for f in file_paths if os.path.exists(f)]
 
-dev_accs = {}
+    parsed_data_dict = {}
 
-parsed_data_dict = {}
-
-for f in file_paths:
-    try:
+    # save results as dictionaries
+    for f in file_paths:
         parsed_data = parse_results_file(f)
-    # except AssertionError:
-    #     continue
-    except FileNotFoundError:
-        continue
-    f = os.path.normpath(f)
-    f = f.split(os.sep)[1]
-    parsed_data_dict[f] = get_json_obj(parsed_data)
-    # if "tabula_muris" not in f: continue
-    dev_accs[f] = get_dev_acc(parsed_data)
+        f = os.path.normpath(f)
+        f = f.split(os.sep)[-2]
+        parsed_data_dict[f] = get_json_obj(parsed_data)
+
+    # save parsed data to a json file
+    with open(save_filename, "w") as f:
+        json.dump(parsed_data_dict, f, indent=4)
 
 
-# save parsed data to a json file
-with open("parsed_results.json", "w") as f:
-    json.dump(parsed_data_dict, f, indent=4)
-
-dev_accs = dict(sorted(dev_accs.items(), key=lambda item: item[1]))
-
-
-
-def get_avg_agg(dev_accs, which):
-    dev_accs = [v for k, v in dev_accs.items() if which(k)]
-    return np.mean(dev_accs)
-
-
-for e, a in dev_accs.items():
-    print(e, a)
-print("#"*10)
-
-for c in [10, 20, 40]:
-    hyp = f"c{c}"
-    print(hyp, get_avg_agg(dev_accs, lambda k: hyp in k))
-print("#"*10)
-
-# # for l in [1, 2]:
-# #     hyp = f"l{l}"
-# #     print(hyp, get_avg_agg(dev_accs, lambda k: hyp in k))
-# # print("#"*10)
-
-# for h in [1, 2, 4, 8]:
-#     hyp = f"h{h}"
-#     print(hyp, get_avg_agg(dev_accs, lambda k: hyp in k))
-# print("#"*10)
-
-# print("p0", get_avg_agg(dev_accs, lambda k: "p0." not in k))
-# for p in ["0.25", "0.5"]:
-#     hyp = f"p{p}"
-#     print(hyp, get_avg_agg(dev_accs, lambda k: hyp in k))
-# print("#"*10)
-
-# print("ffw64", get_avg_agg(dev_accs, lambda k: "ffw" not in k or "ffw64" in k))
-for f in [64, 128, 256]:
-    hyp = f"ffw{f}"
-    print(hyp, get_avg_agg(dev_accs, lambda k: hyp in k))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ckpt_dir', type=str, default="./checkpoints", help='root checkpoint directory containing each exp.name folder')
+    parser.add_argument('--save_filename', type=str, default="parsed_results.json", help='filename to save parsed results to')
+    args = parser.parse_args()
+    main(args.ckpt_dir)
